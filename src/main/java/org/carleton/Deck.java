a01082c3037aac5de2eae1828f1b90a934267cdc
Karl Damus <getaholdofkarl@gmail.com>
2024-09-20 16:38:10 -0400

    R-CODE -RESP-5
    Responsibility ID: 5
    UC Steps: UC-01-1
    Description: Deck shuffles all cards

--- src/main/java/org/carleton/Deck.java
+++ src/main/java/org/carleton/Deck.java
@@ -1,12 +1,22 @@
 package org.carleton;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Deck {
     ArrayList<Card> deck;
 
     public void shuffle() {
+        Random rand = new Random();
 
+        for (int i = 0; i < this.getSize(); i++) {
+            int pos1 = rand.nextInt(this.getSize() - 1);
+            int pos2 = rand.nextInt(this.getSize() - 1);
+
+            Card tempCard = deck.get(pos1);
+            deck.set(pos1, deck.get(pos2));
+            deck.set(pos2, tempCard);
+        }
     }
 
     public ArrayList<Card> getAllCards() {
