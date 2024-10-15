453449f649acd82616fa60f5f9dcc753a41d4a3a
karldamus <getaholdofkarl@gmail.com>
2024-10-15 02:15:02 -0400

    REFAC-008: Main Game Loop

--- src/main/java/org/carleton/Main.java
+++ src/main/java/org/carleton/Main.java
@@ -1,7 +1,25 @@
 package org.carleton;
 
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner input = new Scanner(System.in);
+    private static PrintWriter output = new PrintWriter(System.out);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Game game = new Game();
+        game.initPlayers();
+        game.initDecks();
+        game.getEventDeck().shuffle();
+        game.getAdventureDeck().shuffle();
+        game.dealCards();
+
+
+        while (game.getWinners().isEmpty())
+            game.playersTurn(input, output);
     }
+
 }
